a2c705a2f4e0a42c13fb4f751d4d548c178713fc
Chloe Qiao <qiaochloe@gmail.com>
2025-11-30 22:02:12 -0500

    Update range-analysis tests and diffs

--- tests/mir-opt/range-analysis/bool_u8_cast.rs
+++ tests/mir-opt/range-analysis/bool_u8_cast.rs
@@ -3,8 +3,6 @@
 // EMIT_MIR bool_u8_cast.test.IntegerRange.diff
 fn test(x: bool) -> &'static str {
     // CHECK-LABEL: fn test(
-    // CHECK-NOT: two
-    // CHECK-NOT: unknown
     match x as u8 {
         0 => "null",
         1 => "one",
